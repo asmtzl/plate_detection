2890a27309d1ff7e91782bc0ce45291236640558
asmtzl <asm.tzl06@gmail.com>
2024-06-06 16:53:45 +0300

    add mongo db fastapi functions

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,24 +1,47 @@
-from fastapi import FastAPI, File, UploadFile
-from typing import List
+from fastapi import FastAPI, File, UploadFile, HTTPException, Depends, Request, status
+from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.responses import JSONResponse
 from yolo import detect
-import os
+import os , io
 from PIL import Image
 import numpy as np
-import io
 from fastapi.middleware.cors import CORSMiddleware
 from tempfile import NamedTemporaryFile
+from pydantic import BaseModel
+from hashing import Hash
+from typing import Optional
+from jwttoken import create_access_token
+from oauth import get_current_user
 
 # ngrok http --domain=choice-cheetah-rested.ngrok-free.app http://127.0.0.1:8000
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # İzin vermek istediğiniz origin'leri burada belirleyin
+    allow_origins=["*"],  
     allow_credentials=True,
-    allow_methods=["*"],  # İzin vermek istediğiniz metodları burada belirleyin
-    allow_headers=["*"],  # İzin vermek istediğiniz başlıkları burada belirleyin
+    allow_methods=["*"],  
+    allow_headers=["*"],  
 )
+
+from pymongo import MongoClient
+mongodb_uri = 'mongodb+srv://asmtzl:asmtzl123@platedet.xlpknrp.mongodb.net/?retryWrites=true&w=majority&appName=plateDet'
+port = 8000
+client = MongoClient(mongodb_uri, port)
+db = client["plateUsers"]
+
+class User(BaseModel):
+    username: str
+    company: str
+    password: str
+class Login(BaseModel):
+	username: str
+	password: str
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+class TokenData(BaseModel):
+    username: Optional[str] = None
 
 """UPLOAD_DIR = "uploaded_images"
 os.makedirs(UPLOAD_DIR, exist_ok=True)"""
@@ -76,4 +99,23 @@ async def detect_plate_api_get():
 @app.get("/")
 async def get_hello():
     return {"message": "Hello!"}
+
+@app.post('/register')
+def create_user(request:User):
+	hashed_pass = Hash.bcrypt(request.password)
+	user_object = dict(request)
+	user_object["password"] = hashed_pass
+	user_id = db["user"].insert_one(user_object)
+	# print(user)
+	return {"res":"created"}
+
+@app.post('/login')
+def login(request:OAuth2PasswordRequestForm = Depends()):
+	user = db["user"].find_one({"username":request.username})
+	if not user:
+		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail = f'No user found with this {request.username} username')
+	if not Hash.verify(user["password"],request.password):
+		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail = f'Wrong Username or password')
+	access_token = create_access_token(data={"sub": user["username"] })
+	return {"access_token": access_token, "token_type": "bearer"}
 
